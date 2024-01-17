292215a52d762b33822ddc2dd865d899930395b0
Naygel22 <piotrkozlowski14@gmail.com>
2024-01-17 17:18:05 +0100

    basic version

--- randomAlbums.js
+++ randomAlbums.js
@@ -1,19 +1,27 @@
-const firstRow = document.querySelector('.first-row');
-const secondRow = document.querySelector('.second-row');
-const thirdRow = document.querySelector('.third-row');
+const firstColumn = document.querySelector('.first-column');
+const secondColumn = document.querySelector('.second-column');
+const thirdColumn = document.querySelector('.third-column');
 
 async function getPhotos(){
   const response = await fetch("https://jsonplaceholder.typicode.com/photos");
   const photos = await response.json();
-  const firstPhoto = photos[4500];
-  console.log(firstPhoto);
+  /* const firstPhoto = photos[4500];
+  console.log(firstPhoto); */
+  
+  const photosArray = photos.slice(0,5);
+  console.log(photosArray);
 
+for(let i = 0; i < photosArray.length; i++){
   const imgElement = document.createElement('img');
-  imgElement.src = firstPhoto.url;
-  thirdRow.appendChild(imgElement);
+  imgElement.src = photosArray[i].url;
+  thirdColumn.appendChild(imgElement);
 
-  firstRow.textContent = firstPhoto.id;
-  secondRow.innerHTML = firstPhoto.title;
+  firstColumn.textContent += photosArray[i].id;
+  secondColumn.innerHTML += photosArray[i].title;
+ }
+
+
+  
   //thirdRow.innerHTML = firstPhoto.url;
 }
 getPhotos();
